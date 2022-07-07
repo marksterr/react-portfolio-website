a75caae7a334fae86bc1df15caf8e253739b710a
Mark Dawes <mrkdawes8@gmail.com>
2022-07-07 09:19:13 -0400

    add basic header functionality

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CV from '../../assets/cv.pdf'
+
+// call to action component
+const CTA = () => {
+  return (
+    <div className='cta'>
+      {/* buttons for cv download and contact */}
+      <a href={CV} download className='btn'>Download CV</a>
+      <a href="#contact" classNAme='btn btn-primary'>Let's Talk</a>
+    </div>
+  )
+}
+
+export default CTA
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
 
 // component for portfolio header page
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Mark Dawes</h1>
+        <h5 className="text-light">Aspiring Fullstack Developer</h5>
+        {/* call to action component for cv download and contact */}
+        <CTA />
+
+        {/* personal picture */}
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
