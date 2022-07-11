8f7623e1f7fe43bd2d07f713d432d869c8f606be
Mark Dawes <mrkdawes8@gmail.com>
2022-07-11 09:09:43 -0400

    add styles to testimonials component

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae laborum, quae nobis corporis ut mollitia quos vitae dignissimos in deleniti eius consectetur quaerat! Perspiciatis inventore facere quibusdam deleniti ex aperiam.'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Shatta Wale',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae laborum, quae nobis corporis ut mollitia quos vitae dignissimos in deleniti eius consectetur quaerat! Perspiciatis inventore facere quibusdam deleniti ex aperiam.'
+  },
+  {
+    avatar: AVTR3,
+    name: 'Kwame Despite',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae laborum, quae nobis corporis ut mollitia quos vitae dignissimos in deleniti eius consectetur quaerat! Perspiciatis inventore facere quibusdam deleniti ex aperiam.'
+  },
+  {
+    avatar: AVTR4,
+    name: 'Nana Ama McBrown',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae laborum, quae nobis corporis ut mollitia quos vitae dignissimos in deleniti eius consectetur quaerat! Perspiciatis inventore facere quibusdam deleniti ex aperiam.'
+  },
+]
 
 // component for testimonials page
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        {
+          data.map(({avatar, name, review}, index) => {
+            return (
+              <article key={index} className="testimonial">
+                <div className="client__avatar">
+                  <img src={avatar} />
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                <small className="client__review">{review}</small>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
