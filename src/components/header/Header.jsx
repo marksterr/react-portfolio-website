e573bbbef93ed00f716ce06e1e34bafb7366dc5f
Mark Dawes <mrkdawes8@gmail.com>
2022-09-03 16:58:34 -0400

    change header photo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
 
         {/* personal picture */}
         <div className="me">
-          <img src={ME} alt="me" />
+          <img className="photo" src={ME} alt="me" />
         </div>
 
         <a href="#contact" className='scroll__down'>Scroll Down</a>
